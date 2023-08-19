753611e9b19fc89d8f112bf8978673ac1c9d26be
Jinnu@7093 <dnaveenchakravarthi@gmail.com>
2023-08-19 11:57:12 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>"numbers":[1,2,3,5,8,13]</h1>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
